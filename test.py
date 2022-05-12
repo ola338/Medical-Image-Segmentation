e3c31073f148771e51a45d1b41b87e2715c48b11
ola338 <ola.szalajko2@gmail.com>
2022-05-12 16:15:10 +0200

    create base model

---
+++ test.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from losses import bce_dice_loss
+from u_net import get_unet_128
+import glob
+
+orig_width = 240
+orig_height = 320
+
+threshold = 0.5
+
+
+epochs = 10
+batch_size = 1
+input_size, model = get_unet_128()
+model.load_weights(filepath='weights/best_weights.hdf5')
+
+print(input_size)
+
+
+test_filenames = glob.glob("dataset/test/*.jpg")
+test_filenames = [filename.replace('\\','/').replace('.jpg', '') for filename in test_filenames]
+test_filenames = [filename.split('/')[-1] for filename in test_filenames]
+
+
+print('Predicting on {} samples with batch_size = {}...'.format(len(test_filenames), batch_size))
+for start in tqdm(range(0, len(test_filenames), batch_size)):
+    x_batch = []
+    end = min(start + batch_size, len(test_filenames))
+    ids_test_batch = test_filenames[start:end]
+    for id in ids_test_batch:
+        img = cv2.imread('dataset/test/{}.jpg'.format(id))
+        img = cv2.resize(img, (input_size, input_size))
+        x_batch.append(img)
+    x_batch = np.array(x_batch, np.float32) / 255
+    preds = model.predict_on_batch(x_batch)
+    preds = np.squeeze(preds, axis=3)
+    for index, pred in enumerate(preds):
+        prob = np.array(cv2.resize(pred, (orig_width, orig_height)) > threshold).astype(np.float32) * 255
+        current_filename = ids_test_batch[index]
+        # print(bce_dice_loss(x_batch, prob))
+        cv2.imwrite('dataset/test/seg_pred_att/{}.jpg'.format(id), prob)
+        
+
+print("Done!")
